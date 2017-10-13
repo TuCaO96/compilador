23d148e61cef11ab672c99aaef1fcb11273546ea
Arthur <arthurmr13@hotmail.com>
2017-10-13 15:38:11 -0300

    Begin of the definition of the tokens and symbols, and how they are read

--- src/Lexico.java
+++ src/Lexico.java
@@ -1,105 +1,70 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Lexico {
-	public final int NUM_DEC = 0;
-	public final int NUM_OCT = 1;
-	public final int NUM_HEX = 2;
-	public final int EOF = 3;
-	public final int TERM = 4;
-	public final int ID = 5;
-	public String[] TOKEN  = {"DECIMAL", "OCTAL", "DECIMAL", "EOF", "TERM", "ID"};
-	
-	String entrada = "0x3f8;0724;23;abcd;" + "$";
+    //estados finais
+    public final int EOF = -1;
+    public final int TERM = 0;
+    public final int ID = 1;
+    public final int NUM_INTEIRO = 2;
+    public final int NUM_REAL = 3;
+    public final int OP_SOMA = 4;
+    public final int OP_SUBTRAI = 5;
+    public final int OP_MULTIPLICA = 6;
+    public final int OP_POTENCIA = 7;
+    public final int OP_DIVISAO = 8;
+    public final int OP_IGUAL = 9;
+    public final int OP_OR = 10;
+    public final int OP_AND = 11;
+    public final int ABRE_ARRAY = 12;
+    public final int FECHA_ARRAY = 13;
+    public final int SEPARADOR = 14;
+    public final int ATRIB = 15;
+    public final int ABRE_BLOCO = 16;
+    public final int FECHA_BLOCO = 17;
+    public final int OP_MAIOR = 18;
+    public final int OP_MAIOR_IGUAL = 19;
+    public final int OP_MENOR = 20;
+    public final int OP_MENOR_IGUAL = 21;
+    public final int ABRE_EXPR = 22;
+    public final int FECHA_EXPR = 23;
+    public final int MOD = 24;
+    public final int DIFERENTE = 25;
+    public final int NEGA = 26;
+    public final int CHAR = 27;
+    public final int STRING = 28;
+	//array de simbolos
+	ArrayList<Simbolo> simbolos;
 
-	String lex = "";
+	int pos;
 
-	int pos = 0;
+    String entrada;
 
 	public int anaLex() {
         int estado = 0;
+
 		while (pos < entrada.length()) {
-			char c = getCaracter();
+			char c = getCaracter(entrada);
                         
 			System.out.println("[DEBUG] Caractere lido: " + c);
 
-			switch (estado) {
-			case 0:
-				if (c == '0')
-					estado = 2;
-				else if (c >= '1' && c <= '9')
-					estado = 1;
-				else if(c == '$')
-				    estado = 6;
-				else if(c == ';')
-				    estado = 7;
-				else if(Character.isAlphabetic(c) || c == '_')
-				    estado = 8;
-				else
-					erro(c);
-				break;
-			case 1:
-				if (c >= '0' && c <= '9')
-					estado = 1;
-				else {
-					pos--;
-					return NUM_DEC;
-				}
-				break;
-			case 2:
-				if (c >= '0' && c <= '7')
-					estado = 3;
-				else if (c == 'x' || c == 'X')
-					estado = 4;
-				else {
-					pos--;
-					return NUM_OCT;
-				}
-				break;
-			case 3:
-				if (c >= '0' && c <= '7')
-					estado = 3;
-				else {
-					pos--;
-					return NUM_OCT;
-				}
-				break;
-			case 4:
-				if (Character.isDigit(c) || (c >= 'a' && c <= 'f'))
-					estado = 5;
-				else
-					erro(c);
-				break;
-			case 5:
-				if (Character.isDigit(c) || (c >= 'a' && c <= 'f'))
-					estado = 5;
-				else {
-					pos--;
-					return NUM_HEX;
-				}
-				break;
-            case 6:
-                pos--;
-                return EOF;
-            case 7:
-                pos--;
-                return TERM;
-            case 8:
-                if(Character.isAlphabetic(c) || Character.isDigit(c) || c == '_'){
-                    estado = 8;
-                }
-                else{
-                    pos--;
-                    return ID;
-                }
-                break;
-            default:
-				erro(c);
-			}
+			//INICIO SWITCH DE ESTADOS
 
-			lex = entrada.substring(0, pos);
+			//FIM SWITCH DE ESTADOS
+
+			String lex = entrada.substring(0, pos);
 		}
-		return EOF;
+
+		//fim do arquivo
+		return -1;
 	}
 
-	public char getCaracter() {
+	public char getCaracter(String entrada) {
 		return entrada.charAt(pos++);
 	}
 
@@ -110,13 +75,48 @@ public class Lexico {
 	public Lexico() {
 		int token;
 		token = anaLex();
-        while(token != EOF){
-            System.out.println(TOKEN[token]);
+        while(token != -1){
             token = anaLex();
         }
 
 	}
-	
+
+	public ArrayList<Simbolo> initTabelaSimbolos(){
+		ArrayList<Simbolo> simbolos = new ArrayList<>();
+
+		try {
+
+			File f = new File("src/com/mkyong/data.txt");
+
+			Scanner scan = new Scanner(f);
+
+			BufferedReader b = new BufferedReader(new FileReader(f));
+
+			String linha, posicao, token, lexema;
+
+			//delimitador para nova linha é o ;
+			scan.useDelimiter(Pattern.compile(";"));
+			while (scan.hasNext()) {
+				//proxima linha
+				linha = scan.next();
+				//dividimos a linha por cada virgula encontrada
+				String[] partes = linha.split(",");
+				//e entao salvamos cada informacao encontrada no objeto
+				Simbolo simbolo = new Simbolo();
+				simbolo.setPosicao(Integer.parseInt(partes[0]));
+				simbolo.setToken(partes[1]);
+				simbolo.setLexema(partes[2]);
+				//adiciona novo simbolo na tabela
+				simbolos.add(simbolo);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return simbolos;
+	}
+
 	public static void main(String[] args) {
 		new Lexico();
 	}
---
+++ src/Simbolo.java
@@ -0,0 +1,31 @@
+public class Simbolo {
+    private int Posicao;
+
+    private String Token;
+
+    private String Lexema;
+
+    public int getPosicao() {
+        return Posicao;
+    }
+
+    public void setPosicao(int posicao) {
+        Posicao = posicao;
+    }
+
+    public String getToken() {
+        return Token;
+    }
+
+    public void setToken(String token) {
+        Token = token;
+    }
+
+    public String getLexema() {
+        return Lexema;
+    }
+
+    public void setLexema(String lexema) {
+        Lexema = lexema;
+    }
+}
